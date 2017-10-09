772bad24174bc22af62982ea12f7bc155de44b21
zakirov_m <test@test>
2017-10-09 17:28:38 +0300

    add html

--- webapp/assignment_of_tasks/views.py
+++ webapp/assignment_of_tasks/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Task
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'tasks/post_list.html', {})
+    tasks = Task.objects.all().order_by('-created_date')
+    return render(request, 'tasks/post_list.html', {'tasks' : tasks})
