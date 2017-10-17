1f32b244e42a5ea5434f6729ae443495864bbf96
zakirov_m <test@test>
2017-10-17 11:29:01 +0300

    just need

--- webapp/assignment_of_tasks/views.py
+++ webapp/assignment_of_tasks/views.py
@@ -51,16 +51,17 @@ def task_remove(request, pk):
     task.delete()
     return redirect('task_list')
 
+@login_required
 def add_comment_to_task(request, pk):
     task = get_object_or_404(Task, pk=pk)
     if request.method == "POST":
-        form = CommentForm(request.POST)
-        if form.is_valid():
-            comment = form.save(commit=False)
+        c_form = CommentForm(request.POST)
+        if c_form.is_valid():
+            comment = c_form.save(commit=False)
             comment.task = task
             comment.author = request.user
             comment.save()
             return redirect('task_detail', pk=task.pk)
     else:
-        form = CommentForm()
-    return render(request, 'tasks/add_comment_to_task.html', {'form': form})
+        c_form = CommentForm()
+    return render(request, 'tasks/add_comment_to_task.html', {'c_form': c_form })
