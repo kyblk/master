e4df69704bb6345463db36bfd89cd51387ea524f
zakirov_m <test@test>
2017-10-23 13:58:15 +0300

    get full name instead username in forms

--- webapp/assignment_of_tasks/forms.py
+++ webapp/assignment_of_tasks/forms.py
@@ -1,9 +1,15 @@
 from django import forms
-
+from django.contrib.auth.models import User
 from .models import Task, Comment
+from django.utils.encoding import smart_text
+from django.forms import ModelChoiceField, ModelMultipleChoiceField
+
+class UserFullnameChoiceField(forms.ModelChoiceField):
+    def label_from_instance(self, obj):
+        return smart_text(obj.get_full_name())
 
 class TaskForm(forms.ModelForm):
-
+    assigned_to = UserFullnameChoiceField(queryset=User.objects.all())
     class Meta:
         model = Task
         fields = ('assigned_to', 'title', 'text', 'status')
@@ -15,6 +21,8 @@ class CommentForm(forms.ModelForm):
         fields = ('text','change_state')
 
 class UpdateTask(forms.ModelForm):
+    assigned_to = UserFullnameChoiceField(queryset=User.objects.all())
     class Meta:
         model = Task
         fields = ('assigned_to','status')
+
